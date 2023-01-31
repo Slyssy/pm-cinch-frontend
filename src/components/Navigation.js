115bbce686fd1c3cb331b1c6010c4e59b2ad3c15
Stephen <stephen.lyssy@gmail.com>
2023-01-30 22:24:53 -0600

    Madd navigation bar responsive.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,62 +1,195 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
 import cookie from 'cookie';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
-const checkAuth = () => {
-  const cookies = cookie.parse(document.cookie);
-  // * Checking cookie object to see if cookies.loggedIn is truthy.
-  // console.log(cookies.loggedIn);
-  return cookies['loggedIn'] ? true : false;
-};
-
-const Navigation = () => {
+function ResponsiveAppBar() {
   const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // eslint-disable-next-line
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie);
+    // * Checking cookie object to see if cookies.loggedIn is truthy.
+    // console.log(cookies.loggedIn);
+    return cookies['loggedIn'] ? true : false;
+  };
+
   return (
-    <AppBar position='relative' color='primary'>
-      <Toolbar>
-        <Typography variant='h6' style={{ flexGrow: '1', color: 'white' }}>
-          PM Cinch
-        </Typography>
-        <ul className='nav-list'>
-          {checkAuth() ? (
-            <>
-              <li className='nav-list-item'>
-                <Link to='/'>
-                  <Typography style={{ color: 'white' }}>Dashboard</Typography>
-                </Link>
-              </li>
-              <li className='nav-list-item'>
-                <Link to='/newProject'>
-                  <Typography style={{ color: 'white' }}>
-                    New Project
-                  </Typography>
-                </Link>
-              </li>
-            </>
-          ) : null}
-          <li className='nav-list-item'>
-            <Link to='/newUser'>
-              <Typography style={{ color: 'white' }}>New User</Typography>
-            </Link>
-          </li>
-          <li
-            className='nav-list-item'
-            onClick={() => {
-              document.cookie = cookie.serialize('loggedIn', null, {
-                maxAge: 0,
-              });
-              navigate('/login');
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+          <Typography
+            variant='h6'
+            noWrap
+            component='a'
+            href='/'
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
             }}
           >
-            <Typography style={{ color: 'white' }}>
+            PM Cinch
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {checkAuth() ? (
+                <>
+                  <Link to='/'>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      <Typography textAlign='center'>Dashboard</Typography>
+                    </MenuItem>
+                  </Link>
+                  <Link to='/newProject'>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      <Typography textAlign='center'>New Project</Typography>
+                    </MenuItem>
+                  </Link>
+                </>
+              ) : null}
+              <Link to='/newUser'>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'>New User</Typography>
+                </MenuItem>
+              </Link>
+              <MenuItem
+                onClick={() => {
+                  document.cookie = cookie.serialize(
+                    'loggedIn',
+                    null,
+                    {
+                      maxAge: 0,
+                    },
+                    handleCloseNavMenu
+                  );
+                  navigate('/login');
+                }}
+              >
+                <Typography textAlign='center'>
+                  {checkAuth() ? 'Logout' : 'Login'}
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+          <Typography
+            variant='h5'
+            noWrap
+            component='a'
+            href=''
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            PM Cinch
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {checkAuth() ? (
+              <>
+                <Link to='/'>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    Dashboard
+                  </Button>
+                </Link>
+                <Link to='/newProject'>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    New Project
+                  </Button>
+                </Link>
+              </>
+            ) : null}
+            <Link to='/newUser'>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                New User
+              </Button>
+            </Link>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Button
+              onClick={() => {
+                document.cookie = cookie.serialize('loggedIn', null, {
+                  maxAge: 0,
+                });
+                navigate('/login');
+              }}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
               {checkAuth() ? 'Logout' : 'Login'}
-            </Typography>
-          </li>
-        </ul>
-      </Toolbar>
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
-};
-
-export default Navigation;
+}
+export default ResponsiveAppBar;
---
+++ src/components/Navigation2.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import cookie from 'cookie';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+  // * Checking cookie object to see if cookies.loggedIn is truthy.
+  // console.log(cookies.loggedIn);
+  return cookies['loggedIn'] ? true : false;
+};
+
+const Navigation = () => {
+  const navigate = useNavigate();
+  return (
+    <AppBar position='relative' color='primary'>
+      <Toolbar>
+        <Typography variant='h6' style={{ flexGrow: '1', color: 'white' }}>
+          PM Cinch
+        </Typography>
+        <ul className='nav-list'>
+          {checkAuth() ? (
+            <>
+              <li className='nav-list-item'>
+                <Link to='/'>
+                  <Typography style={{ color: 'white' }}>Dashboard</Typography>
+                </Link>
+              </li>
+              <li className='nav-list-item'>
+                <Link to='/newProject'>
+                  <Typography style={{ color: 'white' }}>
+                    New Project
+                  </Typography>
+                </Link>
+              </li>
+            </>
+          ) : null}
+          <li className='nav-list-item'>
+            <Link to='/newUser'>
+              <Typography style={{ color: 'white' }}>New User</Typography>
+            </Link>
+          </li>
+          <li
+            className='nav-list-item'
+            onClick={() => {
+              document.cookie = cookie.serialize('loggedIn', null, {
+                maxAge: 0,
+              });
+              navigate('/login');
+            }}
+          >
+            <Typography style={{ color: 'white' }}>
+              {checkAuth() ? 'Logout' : 'Login'}
+            </Typography>
+          </li>
+        </ul>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navigation;
