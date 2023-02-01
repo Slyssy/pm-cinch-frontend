d159f58c069316f197113272965126823044bc75
Stephen <stephen.lyssy@gmail.com>
2023-01-31 23:07:21 -0600

    Updated colors and styles on dashboard.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,11 +6,12 @@ import axios from 'axios';
 //# MUI Card Imports
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+// import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+// import { fontWeight } from '@mui/system';
 
 export default function Dashboard(props) {
   console.log(props);
@@ -53,135 +54,153 @@ export default function Dashboard(props) {
   };
 
   return (
-    <main className='dashboard__main'>
-      {projects.map((project) => {
-        return (
-          <Box sx={{ width: '300px' }}>
-            <Card variant='outlined' sx={{ m: 1.5 }}>
-              <React.Fragment>
-                <CardContent>
-                  <Typography
-                    variant='h6'
-                    component='div'
-                    sx={{ mb: 0.5, color: '#2B2D42' }}
-                  >
-                    {project.project_name}
-                  </Typography>
-                  <Typography
-                    variant='body1'
-                    sx={{ mb: 0.5, color: '#da5552' }}
-                  >
-                    {`Revenue: ${USDollar.format(project.original_revenue)}`}
-                  </Typography>
-                  <Typography
-                    variant='body2'
-                    sx={{ mb: 0.5 }}
-                    color='text.secondary'
-                  >
-                    {`Labor Expense: ${USDollar.format(
-                      project.budgeted_labor_expense
-                    )}`}
-                  </Typography>
-                  <Typography
-                    variant='body2'
-                    sx={{ mb: 0.5 }}
-                    color='text.secondary'
-                  >
-                    {`Material Expense: ${USDollar.format(
-                      project.budgeted_material_expense
-                    )}`}
-                  </Typography>
-                  <Typography
-                    variant='body2'
-                    sx={{ mb: 0.5 }}
-                    color='text.secondary'
-                  >
-                    {`Subcontractor Expense: ${USDollar.format(
-                      project.budgeted_subcontractor_expense
-                    )}`}
-                  </Typography>
-                  <Typography
-                    variant='body2'
-                    sx={{ mb: 0.5 }}
-                    color='text.secondary'
-                  >
-                    {`Miscellaneous Expense: ${USDollar.format(
-                      project.budgeted_miscellaneous_expense
-                    )}`}
-                  </Typography>
-                  <Typography variant='body1' sx={{ mb: 0.5 }} color='primary'>
-                    {isNaN(
-                      ((project.original_revenue -
-                        totalExpense([
-                          project.budgeted_labor_expense,
-                          project.budgeted_material_expense,
-                          project.budgeted_miscellaneous_expense,
-                          project.budgeted_subcontractor_expense,
-                        ])) /
-                        project.original_revenue) *
-                        100
-                    )
-                      ? `Gross Margin: TBD`
-                      : `Gross Margin: ${parseFloat(
-                          ((project.original_revenue -
-                            totalExpense([
-                              project.budgeted_labor_expense,
-                              project.budgeted_material_expense,
-                              project.budgeted_miscellaneous_expense,
-                              project.budgeted_subcontractor_expense,
-                            ])) /
-                            project.original_revenue) *
-                            100
-                        ).toFixed(2)} %`}
-                  </Typography>
-                  <Stack
-                    direction='row'
-                    justifyContent='left'
-                    alignItems='center'
-                    spacing={3.5}
-                    marginTop='1em'
-                  >
+    <>
+      <Box className='dashboard__title'>
+        {`Welcome to ${props.user[0].user[0].first_name} ${props.user[0].user[0].last_name}'s `}
+        <span className='text-emphasis'>Project Dashboard</span>
+      </Box>
+      <main className='dashboard__main'>
+        {projects.map((project) => {
+          return (
+            <Box
+              sx={{ width: '300px' }}
+              key={`${project.id}-card`}
+              className='card-holder'
+            >
+              <Card sx={{ m: 0 }}>
+                <React.Fragment>
+                  <CardContent>
                     <Typography
-                      variant='body2'
-                      sx={{ mb: 0.5 }}
-                      color='text.primary'
+                      variant='h6'
+                      component='div'
+                      sx={{ mb: 0.5, color: '#2B2D42', fontWeight: '500' }}
                     >
-                      {`ESD: ${
-                        new Date(project.estimated_start_date).getMonth() + 1
-                      }/${new Date(
-                        project.estimated_start_date
-                      ).getDate()}/${new Date(
-                        project.estimated_start_date
-                      ).getFullYear()}`}
+                      {project.project_name}
+                    </Typography>
+                    <Typography
+                      variant='body1'
+                      sx={{ mb: 0.5, color: '#5d1451', fontWeight: '700' }}
+                    >
+                      {`Revenue: ${USDollar.format(project.original_revenue)}`}
                     </Typography>
                     <Typography
                       variant='body2'
-                      sx={{ mb: 0.5 }}
-                      color='text.primary'
+                      sx={{ mb: 0.5, fontWeight: '700' }}
+                      color='text.secondary'
                     >
-                      {`ECD: ${
-                        new Date(project.estimated_complete_date).getMonth() + 1
-                      }/${new Date(
-                        project.estimated_complete_date
-                      ).getDate()}/${new Date(
-                        project.estimated_complete_date
-                      ).getFullYear()}`}
+                      {`Labor Expense: ${USDollar.format(
+                        project.budgeted_labor_expense
+                      )}`}
                     </Typography>
-                  </Stack>
-                </CardContent>
-                <CardActions>
-                  <Link
-                    to={`/projects/${project.id}`}
-                    onClick={handleClick(project, 'id')}
-                  >
-                    <Button size='small'>Project Details</Button>
-                  </Link>
-                </CardActions>
-              </React.Fragment>
-            </Card>
-          </Box>
-        );
-      })}
-    </main>
+                    <Typography
+                      variant='body2'
+                      sx={{ mb: 0.5, fontWeight: '700' }}
+                      color='text.secondary'
+                    >
+                      {`Material Expense: ${USDollar.format(
+                        project.budgeted_material_expense
+                      )}`}
+                    </Typography>
+                    <Typography
+                      variant='body2'
+                      sx={{ mb: 0.5, fontWeight: '700' }}
+                      color='text.secondary'
+                    >
+                      {`Subcontractor Expense: ${USDollar.format(
+                        project.budgeted_subcontractor_expense
+                      )}`}
+                    </Typography>
+                    <Typography
+                      variant='body2'
+                      sx={{ mb: 0.5, fontWeight: '700' }}
+                      color='text.secondary'
+                    >
+                      {`Miscellaneous Expense: ${USDollar.format(
+                        project.budgeted_miscellaneous_expense
+                      )}`}
+                    </Typography>
+                    <Typography
+                      variant='body1'
+                      sx={{ mb: 0.5, color: '#ffb703', fontWeight: '700' }}
+                    >
+                      {isNaN(
+                        ((project.original_revenue -
+                          totalExpense([
+                            project.budgeted_labor_expense,
+                            project.budgeted_material_expense,
+                            project.budgeted_miscellaneous_expense,
+                            project.budgeted_subcontractor_expense,
+                          ])) /
+                          project.original_revenue) *
+                          100
+                      )
+                        ? `Gross Margin: TBD`
+                        : `Gross Margin: ${parseFloat(
+                            ((project.original_revenue -
+                              totalExpense([
+                                project.budgeted_labor_expense,
+                                project.budgeted_material_expense,
+                                project.budgeted_miscellaneous_expense,
+                                project.budgeted_subcontractor_expense,
+                              ])) /
+                              project.original_revenue) *
+                              100
+                          ).toFixed(2)} %`}
+                    </Typography>
+                    <Stack
+                      direction='row'
+                      justifyContent='left'
+                      alignItems='center'
+                      spacing={3.5}
+                      marginTop='1em'
+                    >
+                      <Typography
+                        variant='body2'
+                        sx={{ mb: 0.5 }}
+                        color='text.primary'
+                      >
+                        {`ESD: ${
+                          new Date(project.estimated_start_date).getMonth() + 1
+                        }/${new Date(
+                          project.estimated_start_date
+                        ).getDate()}/${new Date(
+                          project.estimated_start_date
+                        ).getFullYear()}`}
+                      </Typography>
+                      <Typography
+                        variant='body2'
+                        sx={{ mb: 0.5 }}
+                        color='text.primary'
+                      >
+                        {`ECD: ${
+                          new Date(project.estimated_complete_date).getMonth() +
+                          1
+                        }/${new Date(
+                          project.estimated_complete_date
+                        ).getDate()}/${new Date(
+                          project.estimated_complete_date
+                        ).getFullYear()}`}
+                      </Typography>
+                    </Stack>
+                    <Link
+                      to={`/projects/${project.id}`}
+                      onClick={handleClick(project, 'id')}
+                    >
+                      <Button
+                        size='small'
+                        sx={{ color: '#5d1451', fontWeight: '700' }}
+                      >
+                        Project Details
+                      </Button>
+                    </Link>
+                  </CardContent>
+                  {/* <CardActions></CardActions> */}
+                </React.Fragment>
+              </Card>
+            </Box>
+          );
+        })}
+      </main>
+    </>
   );
 }
